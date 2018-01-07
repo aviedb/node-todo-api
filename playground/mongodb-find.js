62a80d29fe96694af329a9d94469f99811114b46
aviedb <aviedbachmid.ab@gmail.com>
2018-01-07 21:20:54 +0800

    Typo Fixes

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -14,18 +14,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log('Unable to fetch data from Todos', err)
     // })
 
-    // db.collection('Users').insertOne({
-    //     name: 'Zakwan',
-    //     age: 18,
-    //     location: 'Medan'
-    // }, (err, results) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user.', err)
-    //     }
-
-    //     console.log(JSON.stringify(results.ops, undefined, 2))
-    // })
-
     db.collection('Users').find({location: 'Medan'}).toArray().then((docs) => {
         console.log('Users:')
         console.log(JSON.stringify(docs, undefined, 2))
