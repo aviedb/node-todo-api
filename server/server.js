fb094311aca16a0093a1e7ad147cd4d5845cd2fa
aviedb <aviedbachmid.ab@gmail.com>
2018-01-09 08:38:02 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -27,6 +28,24 @@ app.get('/todos', (req, res) => {
         res.send({todos})
     }, (err) => {
         res.status(400).send(err)
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+    
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            res.status(404).send()
+        }
+
+        res.send({todo})
+    }).catch((e) => {
+        res.status(404).send()
     })
 })
 
