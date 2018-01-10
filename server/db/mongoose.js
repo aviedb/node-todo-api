c45ce686ecfb70a4f3fe15c9f72ddd4aa8b48998
aviedb <aviedbachmid.ab@gmail.com>
2018-01-10 09:15:51 +0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect('mongodb://aviedb:123Sembarang@ds249727.mlab.com:49727/todo-app-avied' || 'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true
 })
 
