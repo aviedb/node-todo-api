4c448a352f4778bc4184a7e18e4a8ccf60eec4bf
aviedb <aviedbachmid.ab@gmail.com>
2018-01-11 10:31:01 +0800

    Add POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,33 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
     password: {
         type: String,
-        default: null
-    }
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
-
-// For documentation:
-// var newUser = new User({
-//     email: 'aviedbachmid.ab@gmail.com'
-// })
-
-// newUser.save().then((res) => {
-//     console.log('User Saved\n', JSON.stringify(res, undefined, 2))
-// }, (err) => {
-//     console.log('Unable to save user\n', err)
-// })
 
 module.exports = {User}
